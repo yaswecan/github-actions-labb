d89119c7ad22c600e34ef13fbd2a6aee5fafee1a
Eden <yacinelahjaily@gmail.com>
2025-11-21 11:35:31 +0100

    add test

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,4 @@
+
+export default function Button() {
+    return <button>Cliquez Ici</button>
+}
---
+++ src/components/button/Button.test.jsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react"
+import { describe, expect , test} from "vitest"
+import Button from "./Button"
+
+describe("button testing", () => {
+    test("Le boutton affiche le texte 'Cliquez ici'", () => {
+    render(<Button/>)
+    const expectedText = screen.getByText("Cliquez Ici")
+    expect(expectedText).toBeInTheDocument()
+    })
+})
